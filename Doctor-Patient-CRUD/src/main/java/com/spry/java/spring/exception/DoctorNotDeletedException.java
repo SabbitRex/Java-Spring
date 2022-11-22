5831494912174994f2617b6dd95f57bdba33ce38
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-11-23 02:01:05 +0700

    Add files via upload

--- Doctor-Patient-CRUD/src/main/java/com/spry/java/spring/exception/DoctorNotDeletedException.java
+++ Doctor-Patient-CRUD/src/main/java/com/spry/java/spring/exception/DoctorNotDeletedException.java
@@ -1,1 +1,23 @@
+package com.spry.java.spring.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+public class DoctorNotDeletedException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 223406952505246000L;
+	private Object fieldValue;
+	
+	public DoctorNotDeletedException(Object fieldValue) {
+		super(String.format("Could not delete doctor with ID : '%s'.", fieldValue));
+		this.fieldValue = fieldValue;
+	}
+	
+	public Object getFieldValue() {
+		return fieldValue;
+	}
+}
---
+++ Doctor-Patient-CRUD/src/main/java/com/spry/java/spring/exception/ResourceNotFoundException.java
@@ -0,0 +1,32 @@
+package com.spry.java.spring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 2192819893732070509L;
+	private String resourceName;
+	private String fieldName;
+	private Object fieldValue;
+	
+	public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+		super(String.format("Resource %s could not be found. [%s : '%s']", resourceName, fieldName, fieldValue));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue = fieldValue;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public Object getFieldValue() {
+		return fieldValue;
+	}
+}
