9c1d4ccbe1a2f9977e06361d1e4677a438073c54
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-11-23 02:02:15 +0700

    Add files via upload

--- Doctor-Patient-CRUD/src/main/java/com/spry/java/spring/repository/PatientRepository.java
+++ Doctor-Patient-CRUD/src/main/java/com/spry/java/spring/repository/PatientRepository.java
@@ -3,9 +3,9 @@ package com.spry.java.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spry.java.spring.entity.Doctor;
+import com.spry.java.spring.entity.Patient;
 
 @Repository
-public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
 }
